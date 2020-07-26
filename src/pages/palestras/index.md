34816f18416989edbf6648fd423ec9b9a484479c
hel rabelo <helrabelo@gmail.com>
2020-07-26 09:07:50 -0300

    adicionando página de Palestras

--- src/pages/palestras/index.md
+++ src/pages/palestras/index.md
@@ -1,4 +1,14 @@
 ---
 templateKey: palestras
-title: Palestras Page
+pageTitle: Palestras
+current:
+  palestra: Palestra 1
+  palestrante: Palestrante 1
+  data: 01/01/2020
+  horario: 17`:`10
+past:
+  palestra: Palestra 1
+  palestrante: Palestrante 1
+  data: 01/01/2020
+  horario: 17`:`10
 ---
--- src/templates/palestras.js
+++ src/templates/palestras.js
@@ -1,27 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
+import Container from '../components/Container';
+import SectionTitle from '../components/SectionTitle';
 
 import Layout from '../components/Layout';
 import Features from '../components/Features';
 import BlogRoll from '../components/BlogRoll';
 
-export const PalestrasPageTemplate = ({ title }) => (
-  <div>
-    <h1>{title}</h1>
-  </div>
+export const PalestrasPageTemplate = ({ pageTitle, current, past }) => (
+  <>
+    <Container>
+      <SectionTitle>{pageTitle}</SectionTitle>
+    </Container>
+    <p>{current.palestra}</p>
+    <p>{current.palestrante}</p>
+    <p>{current.data}</p>
+    <p>{current.horario}</p>
+    <p>{past.palestra}</p>
+    <p>{past.palestrante}</p>
+    <p>{past.data}</p>
+    <p>{past.horario}</p>
+  </>
 );
 
 PalestrasPageTemplate.propTypes = {
-  title: PropTypes.string,
+  pageTitle: PropTypes.string,
+  current: PropTypes.shape({
+    palestra: PropTypes.string,
+    palestrante: PropTypes.string,
+    data: PropTypes.string,
+    horario: PropTypes.string,
+  }),
+  past: PropTypes.shape({
+    palestra: PropTypes.string,
+    palestrante: PropTypes.string,
+    data: PropTypes.string,
+    horario: PropTypes.string,
+  }),
 };
 
 const PalestrasPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark;
+  console.log(frontmatter);
 
   return (
     <Layout>
-      <PalestrasPageTemplate title={frontmatter.title} />
+      <PalestrasPageTemplate
+        pageTitle={frontmatter.pageTitle}
+        current={frontmatter.current}
+        past={frontmatter.past}
+      />
     </Layout>
   );
 };
@@ -40,7 +69,19 @@ export const pageQuery = graphql`
   query PalestrasPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "palestras" } }) {
       frontmatter {
-        title
+        pageTitle
+        current {
+          palestra
+          palestrante
+          data
+          horario
+        }
+        past {
+          palestra
+          palestrante
+          data
+          horario
+        }
       }
     }
   }
