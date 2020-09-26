b391768d908c089e0ab8c22394352be9b8505e11
hel rabelo <helrabelo@gmail.com>
2020-09-26 09:48:10 -0300

    working on netlify cms config for content

--- src/templates/atividades.js
+++ src/templates/atividades.js
@@ -3,13 +3,33 @@ import PropTypes from 'prop-types';
 import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
+import Container from '../components/Container';
+import SectionTitle from '../components/SectionTitle';
 import Features from '../components/Features';
 import PostList from '../components/PostList';
 
+
+import unified from 'unified'
+import parse from 'remark-parse'
+import remark2react from 'remark-react'
+import breaks from 'remark-breaks'
+
+
 export const AtividadesPageTemplate = ({ title }) => (
-  <div>
-    <h1>{title}</h1>
-  </div>
+  <section id="cefa-activities">
+    <Container>
+      <div className="index">
+        <h3>Índice</h3>
+        <Link to="#apresentacao">1. Apresentação</Link>
+        <Link to="#board">
+          2. Diretoria, conselho consultivo e coordenadorias
+        </Link>
+        <Link to="#estatuto">3. Estatuto</Link>
+        <Link to="#regime-interno">4. Regimento Interno</Link>
+        <Link to="#localizacao">5. Localização</Link>
+      </div>
+    </Container>
+  </section >
 );
 
 AtividadesPageTemplate.propTypes = {
