0a3e85d516d555a2d6caa3c3040fc0ea1f1c2ce4
hel rabelo <helrabelo@gmail.com>
2023-01-16 06:48:54 -0300

    add horarios content

--- components/Tab/Horarios.tsx
+++ components/Tab/Horarios.tsx
@@ -1,10 +1,173 @@
 import { useState } from "react";
 import classnames from "classnames";
 
-export default function Tabs() {
-  const [activeTab, setActiveTab] = useState("dom");
+type Activity = {
+  value?: string;
+  label?: string;
+};
 
-  const changeTab = (newTab: string) => setActiveTab(newTab);
+type DailyActivities = {
+  man?: Activity[];
+  tar?: Activity[];
+  noi?: Activity[];
+};
+
+type WeeklyActivities = {
+  dom: DailyActivities;
+  seg: DailyActivities;
+  ter: DailyActivities;
+  qua: DailyActivities;
+  qui: DailyActivities;
+  sex: DailyActivities;
+  sab: DailyActivities;
+};
+
+export default function Tabs() {
+  const horarios: WeeklyActivities = {
+    dom: {
+      man: [],
+      tar: [
+        {
+          value: "16:00",
+          label: "Estudo das Mediúnicas",
+        },
+        {
+          value: "16:30",
+          label: "Atendimento Espiritual - AE",
+        },
+      ],
+      noi: [
+        {
+          value: "18:00",
+          label: "Hora da Prece",
+        },
+      ],
+    },
+    seg: {
+      man: [],
+      tar: [],
+      noi: [
+        {
+          value: "19:00 às 20:00",
+          label: "Orientação Fraterna",
+        },
+        {
+          value: "19:30",
+          label: "Estudos Doutrinatários (ESDE e EADE)",
+        },
+      ],
+    },
+    ter: {
+      man: [],
+      tar: [
+        {
+          value: "16:00 às 20:30",
+          label: "Orientação Fraterna",
+        },
+        {
+          value: "16:00",
+          label: "Estudos das Mediúnicas",
+        },
+      ],
+      noi: [
+        {
+          value: "19:00",
+          label: "Estudos das Mediúnicas",
+        },
+        {
+          value: "19:30",
+          label: "Atendimento Espiritual - AE / Passe Virtual",
+        },
+      ],
+    },
+    qua: {
+      man: [],
+      tar: [],
+      noi: [
+        {
+          value: "19:00 às 20:00",
+          label: "Orientação Fraterna",
+        },
+      ],
+    },
+    qui: {
+      man: [],
+      tar: [],
+      noi: [
+        {
+          value: "18:00 às 20:00",
+          label: "Orientação Fraterna",
+        },
+        {
+          value: "19:00",
+          label: "Estudo das Mediúnicas",
+        },
+        {
+          value: "19:30",
+          label: "Atendimento Espiritual - AE / Passe Virtual",
+        },
+      ],
+    },
+    sex: { man: [], tar: [], noi: [] },
+    sab: {
+      man: [],
+      tar: [
+        {
+          value: "15:00",
+          label: "Evangelização Infantojuvenil / Passe Virtual",
+        },
+        {
+          value: "17:30",
+          label: "Estudos Doutrinatários (ESDE e EADE)",
+        },
+      ],
+      noi: [],
+    },
+  };
+
+  type DaysOfWeek = "dom" | "seg" | "ter" | "qua" | "qui" | "sex" | "sab";
+
+  const [activeTab, setActiveTab] = useState<DaysOfWeek>("dom");
+
+  const changeTab = (newTab: DaysOfWeek) => setActiveTab(newTab);
+
+  const renderTabContent = () => (
+    <div className="flex gap-4 w-full">
+      <div className="w-full p-4 border border-black">
+        <p className="font-[900]">Manhã</p>
+        {activeTab &&
+          horarios[activeTab].man &&
+          horarios[activeTab]?.man?.map(({ value, label }: Activity) => (
+            <div className="flex gap-2" key={label}>
+              <p className="font-[900]">{value}</p>
+              <p>{label}</p>
+            </div>
+          ))}
+      </div>
+      <div className="w-full p-4 border border-black">
+        <p className="font-[900]">Tarde</p>
+        {activeTab &&
+          horarios[activeTab].tar &&
+          horarios[activeTab]?.tar?.map(({ value, label }: Activity) => (
+            <div className="flex gap-2" key={label}>
+              <p className="font-[900]">{value}</p>
+              <p>{label}</p>
+            </div>
+          ))}
+      </div>
+      <div className="w-full p-4 border border-black">
+        <p className="font-[900]">Noite</p>
+        {activeTab &&
+          horarios[activeTab].noi &&
+          horarios[activeTab]?.noi?.map(({ value, label }: Activity) => (
+            <div className="flex gap-2" key={label}>
+              <p className="font-[900]">{value}</p>
+              <p>{label}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 
   return (
     <div className="flex flex-col gap-8">
@@ -96,8 +259,8 @@ export default function Tabs() {
         </button>
       </div>
 
-      {/* Atividades */}
-      
+      {/* Tab Content */}
+      {renderTabContent()}
     </div>
   );
 }
