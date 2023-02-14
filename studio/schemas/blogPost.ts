4757c793b581ab9010e998161532209deefbc5dc
hel rabelo <helrabelo@gmail.com>
2023-02-14 11:27:25 -0300

    setup sanity.io

---
+++ studio/schemas/blogPost.ts
@@ -0,0 +1,54 @@
+export default {
+  name: 'blogPost',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule: { required: () => any; }) => Rule.required(),
+    },
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule: { required: () => any; }) => Rule.required(),
+    },
+    {
+      name: 'featuredImage',
+      title: 'Featured Image',
+      type: 'image',
+      validation: (Rule: { required: () => any; }) => Rule.required(),
+    },
+    {
+      name: 'contentBlocks',
+      title: 'Content Blocks',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'H2', value: 'h2' },
+            { title: 'H3', value: 'h3' },
+            { title: 'Quote', value: 'blockquote' },
+            { title: 'Bullet List', value: 'bulletList' },
+            { title: 'Numbered List', value: 'numberedList' },
+          ],
+        },
+        {
+          type: 'image',
+          options: {
+            hotspot: true,
+          },
+        },
+      ],
+      validation: (Rule: { required: () => any; }) => Rule.required(),
+    },
+  ],
+};
---
+++ studio/schemas/index.ts
@@ -0,0 +1,7 @@
+import atividades from './atividades'
+import about from './about'
+import blogPost from './blogPost'
+import musicalGroup from './musical-group'
+import homepage from './homepage'
+
+export const schemaTypes = [atividades, about, blogPost, homepage, musicalGroup]
