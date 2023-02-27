b0bcf18fc544c1851870ce31b85c561407c6d91b
hel rabelo <helrabelo@gmail.com>
2023-02-27 14:37:26 -0300

    add validations and update blogPost schema

--- studio/schemas/blogPost.ts
+++ studio/schemas/blogPost.ts
@@ -10,20 +10,21 @@ export default {
       options: {
         source: 'title',
         maxLength: 96,
+        slugify: (input: any) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
       },
-      validation: (Rule: { required: () => any; }) => Rule.required(),
+      validation: (Rule: {required: () => any}) => Rule.required(),
     },
     {
       name: 'title',
       title: 'Title',
       type: 'string',
-      validation: (Rule: { required: () => any; }) => Rule.required(),
+      validation: (Rule: {required: () => any}) => Rule.required(),
     },
     {
       name: 'featuredImage',
       title: 'Featured Image',
       type: 'image',
-      validation: (Rule: { required: () => any; }) => Rule.required(),
+      validation: (Rule: {required: () => any}) => Rule.required(),
     },
     {
       name: 'contentBlocks',
@@ -33,12 +34,12 @@ export default {
         {
           type: 'block',
           styles: [
-            { title: 'Normal', value: 'normal' },
-            { title: 'H2', value: 'h2' },
-            { title: 'H3', value: 'h3' },
-            { title: 'Quote', value: 'blockquote' },
-            { title: 'Bullet List', value: 'bulletList' },
-            { title: 'Numbered List', value: 'numberedList' },
+            {title: 'Normal', value: 'normal'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'Quote', value: 'blockquote'},
+            {title: 'Bullet List', value: 'bulletList'},
+            {title: 'Numbered List', value: 'numberedList'},
           ],
         },
         {
@@ -48,7 +49,7 @@ export default {
           },
         },
       ],
-      validation: (Rule: { required: () => any; }) => Rule.required(),
+      validation: (Rule: {required: () => any}) => Rule.required(),
     },
   ],
-};
+}
