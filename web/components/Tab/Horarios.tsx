615494d6d67ddb3df54e112026a7fe4eb29b3d98
hel rabelo <helrabelo@gmail.com>
2023-04-03 13:56:10 -0300

    integrate sanity to horarios page

--- web/components/Tab/Horarios.tsx
+++ web/components/Tab/Horarios.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import classnames from "classnames";
+import {useState} from 'react';
+import classnames from 'classnames';
 
 type Activity = {
-  value?: string;
-  label?: string;
+  title?: string;
+  time?: string;
 };
 
 type DailyActivities = {
@@ -13,156 +13,65 @@ type DailyActivities = {
 };
 
 type WeeklyActivities = {
-  dom: DailyActivities;
-  seg: DailyActivities;
-  ter: DailyActivities;
-  qua: DailyActivities;
-  qui: DailyActivities;
-  sex: DailyActivities;
-  sab: DailyActivities;
+  Domingo: DailyActivities;
+  Segunda: DailyActivities;
+  Terça: DailyActivities;
+  Quarta: DailyActivities;
+  Quinta: DailyActivities;
+  Sexta: DailyActivities;
+  Sábado: DailyActivities;
 };
 
-export default function Tabs() {
-  const horarios: WeeklyActivities = {
-    dom: {
-      man: [],
-      tar: [
-        {
-          value: "16:00",
-          label: "Estudo das Mediúnicas",
-        },
-        {
-          value: "16:30",
-          label: "Atendimento Espiritual - AE",
-        },
-      ],
-      noi: [
-        {
-          value: "18:00",
-          label: "Hora da Prece",
-        },
-      ],
-    },
-    seg: {
-      man: [],
-      tar: [],
-      noi: [
-        {
-          value: "19:00 às 20:00",
-          label: "Orientação Fraterna",
-        },
-        {
-          value: "19:30",
-          label: "Estudos Doutrinatários (ESDE e EADE)",
-        },
-      ],
-    },
-    ter: {
-      man: [],
-      tar: [
-        {
-          value: "16:00 às 20:30",
-          label: "Orientação Fraterna",
-        },
-        {
-          value: "16:00",
-          label: "Estudos das Mediúnicas",
-        },
-      ],
-      noi: [
-        {
-          value: "19:00",
-          label: "Estudos das Mediúnicas",
-        },
-        {
-          value: "19:30",
-          label: "Atendimento Espiritual - AE / Passe Virtual",
-        },
-      ],
-    },
-    qua: {
-      man: [],
-      tar: [],
-      noi: [
-        {
-          value: "19:00 às 20:00",
-          label: "Orientação Fraterna",
-        },
-      ],
-    },
-    qui: {
-      man: [],
-      tar: [],
-      noi: [
-        {
-          value: "18:00 às 20:00",
-          label: "Orientação Fraterna",
-        },
-        {
-          value: "19:00",
-          label: "Estudo das Mediúnicas",
-        },
-        {
-          value: "19:30",
-          label: "Atendimento Espiritual - AE / Passe Virtual",
-        },
-      ],
-    },
-    sex: { man: [], tar: [], noi: [] },
-    sab: {
-      man: [],
-      tar: [
-        {
-          value: "15:00",
-          label: "Evangelização Infantojuvenil / Passe Virtual",
-        },
-        {
-          value: "17:30",
-          label: "Estudos Doutrinatários (ESDE e EADE)",
-        },
-      ],
-      noi: [],
-    },
-  };
+type TabsProps = {
+  weeklyActivities: WeeklyActivities;
+};
 
-  type DaysOfWeek = "dom" | "seg" | "ter" | "qua" | "qui" | "sex" | "sab";
+export default function Tabs({weeklyActivities}: TabsProps) {
+  type DaysOfWeek =
+    | 'Domingo'
+    | 'Segunda'
+    | 'Terça'
+    | 'Quarta'
+    | 'Quinta'
+    | 'Sexta'
+    | 'Sábado';
 
-  const [activeTab, setActiveTab] = useState<DaysOfWeek>("dom");
+  const [activeTab, setActiveTab] = useState<DaysOfWeek>('Domingo');
 
   const changeTab = (newTab: DaysOfWeek) => setActiveTab(newTab);
 
   const renderTabContent = () => (
-    <div className="flex flex-col md:flex-row gap-4 w-full">
+    <div className="flex flex-col md:flex-col gap-4 w-full">
       <div className="w-full p-4 border border-black">
         <p className="font-[900]">Manhã</p>
         {activeTab &&
-          horarios[activeTab].man &&
-          horarios[activeTab]?.man?.map(({ value, label }: Activity) => (
-            <div className="flex gap-2" key={label}>
-              <p className="font-[900]">{value}</p>
-              <p>{label}</p>
+          weeklyActivities[activeTab].man &&
+          weeklyActivities[activeTab]?.man?.map(({time, title}: Activity) => (
+            <div className="flex gap-2" key={title}>
+              <p className="font-[900]">{time}</p>
+              <p>{title}</p>
             </div>
           ))}
       </div>
       <div className="w-full p-4 border border-black">
         <p className="font-[900]">Tarde</p>
         {activeTab &&
-          horarios[activeTab].tar &&
-          horarios[activeTab]?.tar?.map(({ value, label }: Activity) => (
-            <div className="flex gap-2" key={label}>
-              <p className="font-[900]">{value}</p>
-              <p>{label}</p>
+          weeklyActivities[activeTab].tar &&
+          weeklyActivities[activeTab]?.tar?.map(({time, title}: Activity) => (
+            <div className="flex gap-2" key={title}>
+              <p className="font-[900]">{time}</p>
+              <p>{title}</p>
             </div>
           ))}
       </div>
       <div className="w-full p-4 border border-black">
         <p className="font-[900]">Noite</p>
         {activeTab &&
-          horarios[activeTab].noi &&
-          horarios[activeTab]?.noi?.map(({ value, label }: Activity) => (
-            <div className="flex gap-2" key={label}>
-              <p className="font-[900]">{value}</p>
-              <p>{label}</p>
+          weeklyActivities[activeTab].noi &&
+          weeklyActivities[activeTab]?.noi?.map(({time, title}: Activity) => (
+            <div className="flex gap-2" key={title}>
+              <p className="font-[900]">{time}</p>
+              <p>{title}</p>
             </div>
           ))}
       </div>
@@ -173,90 +82,28 @@ export default function Tabs() {
     <div className="flex flex-col gap-8">
       {/* Tabs */}
       <div className="flex overflow-scroll">
-        <button
-          onClick={() => changeTab("dom")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "dom",
-              "text-black hover:bg-primary-800": activeTab !== "dom",
-            }
-          )}
-        >
-          Domingo
-        </button>
-        <button
-          onClick={() => changeTab("seg")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "seg",
-              "text-black hover:bg-primary-800": activeTab !== "seg",
-            }
-          )}
-        >
-          Segunda
-        </button>
-        <button
-          onClick={() => changeTab("ter")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "ter",
-              "text-black hover:bg-primary-800": activeTab !== "ter",
-            }
-          )}
-        >
-          Terça
-        </button>
-        <button
-          onClick={() => changeTab("qua")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "qua",
-              "text-black hover:bg-primary-800": activeTab !== "qua",
-            }
-          )}
-        >
-          Quarta
-        </button>
-        <button
-          onClick={() => changeTab("qui")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "qui",
-              "text-black hover:bg-primary-800": activeTab !== "qui",
-            }
-          )}
-        >
-          Quinta
-        </button>
-        <button
-          onClick={() => changeTab("sab")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "sab",
-              "text-black hover:bg-primary-800": activeTab !== "sab",
-            }
-          )}
-        >
-          Sexta
-        </button>
-        <button
-          onClick={() => changeTab("sab")}
-          className={classnames(
-            "w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]",
-            {
-              "text-white bg-primary": activeTab === "sab",
-              "text-black hover:bg-primary-800": activeTab !== "sab",
-            }
-          )}
-        >
-          Sábado
-        </button>
+        {[
+          'Domingo',
+          'Segunda',
+          'Terça',
+          'Quarta',
+          'Quinta',
+          'Sexta',
+          'Sábado',
+        ].map((day) => (
+          <button
+            onClick={() => changeTab(day as DaysOfWeek)}
+            className={classnames(
+              'w-full border border-black p-4 font-[600]  hover:text-white text-xl transition border-r-none text-[20px] leading-[29px]',
+              {
+                'text-white bg-primary': activeTab === day,
+                'text-black hover:bg-primary-800': activeTab !== day,
+              }
+            )}
+          >
+            {day}
+          </button>
+        ))}
       </div>
 
       {/* Tab Content */}
--- web/pages/horarios.tsx
+++ web/pages/horarios.tsx
@@ -1,227 +1,105 @@
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
 
-import SectionTitle from "../components/SectionTitle";
-import Link from "next/link";
+import SectionTitle from '../components/SectionTitle';
+import Link from 'next/link';
 
-import { HorariosTab } from "../components/Tab";
+import {HorariosTab} from '../components/Tab';
+import {ActivitiesList} from '../types';
 
-export default function Horarios() {
+type HorariosProps = {
+  atividadesData: ActivitiesList;
+};
+
+function transformActivities(json) {
+  const daysOfWeek = {
+    Domingo: [],
+    Segunda: [],
+    Terça: [],
+    Quarta: [],
+    Quinta: [],
+    Sexta: [],
+    Sábado: [],
+  };
+
+  const activities = json.activitiesList;
+  activities.forEach((activity) => {
+    const subActivities = activity.subActivities;
+    subActivities.forEach((subActivity) => {
+      const hours = subActivity.hours;
+      hours.forEach((hour) => {
+        const [hourValue, minuteValue] = hour.time.split(':');
+        const time = parseInt(hourValue) + parseInt(minuteValue) / 60;
+
+        if (time >= 0 && time < 12) {
+          daysOfWeek[hour.dayOfWeek]['man'] =
+            daysOfWeek[hour.dayOfWeek]['man'] || [];
+          daysOfWeek[hour.dayOfWeek]['man'].push({
+            title: subActivity.title,
+            time: hour.time,
+          });
+        } else if (time >= 12 && time < 18) {
+          daysOfWeek[hour.dayOfWeek]['tar'] =
+            daysOfWeek[hour.dayOfWeek]['tar'] || [];
+          daysOfWeek[hour.dayOfWeek]['tar'].push({
+            title: subActivity.title,
+            time: hour.time,
+          });
+        } else {
+          daysOfWeek[hour.dayOfWeek]['noi'] =
+            daysOfWeek[hour.dayOfWeek]['noi'] || [];
+          daysOfWeek[hour.dayOfWeek]['noi'].push({
+            title: subActivity.title,
+            time: hour.time,
+          });
+        }
+      });
+    });
+  });
+
+  return daysOfWeek;
+}
+
+export default function Horarios({atividadesData}: HorariosProps) {
+  const {activitiesList} = atividadesData;
+
   return (
     <Layout>
       <div className="container mx-auto flex flex-col gap-8 px-8 lg:px-0">
         <SectionTitle as="h2">Horários</SectionTitle>
         {/* Horários */}
-        <HorariosTab />
+        <HorariosTab weeklyActivities={transformActivities(atividadesData)} />
         {/* Atividades */}
         <SectionTitle as="h2" size="large">
           Atividades
         </SectionTitle>
 
-        {/* 1.0 - Estudo da Doutrina Espírita */}
-        <SectionTitle as="h3">1.0 - Estudo da Doutrina Espírita</SectionTitle>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-fit gap-4">
-          {/* 1.1 - Estudo Preparatório (EPE) */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              1.1 - Estudo Preparatório (EPE)
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
+        {Array.isArray(activitiesList) &&
+          activitiesList.map(({title, subActivities}, activityIndex) => (
+            <>
+              <SectionTitle as="h3">
+                {activityIndex + 1} - {title}
+              </SectionTitle>
+              <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-fit gap-4">
+                {Array.isArray(subActivities) &&
+                  subActivities.map((subActivity, subActivityIndex) => (
+                    <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
+                      <SectionTitle as="h4" size="small">
+                        {activityIndex + 1}.{subActivityIndex + 1} -{' '}
+                        {subActivity.title}
+                      </SectionTitle>
+                      <div className="flex flex-col gap-2">
+                        {subActivity?.hours?.map(({dayOfWeek, time}) => (
+                          <p>
+                            {dayOfWeek} - {time}
+                          </p>
+                        ))}
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </>
+          ))}
 
-          {/* 1.2 - Estudo Preparatório */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              1.2 - Estudo Sistematizado da Doutrina Espírita (ESDE)
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 1.3 - Estudo Aprofundado da Doutrina Espírita (EADE) */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              1.3 - Estudo Aprofundado da Doutrina Espírita (EADE)
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 1.4 - Estudo de Obras Básicas e Complementares */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              1.4 - Estudo de Obras Básicas e Complementares
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 1.5 - Palestras Públicas */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              1.5 - Palestras Públicas
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Quarta - 19:30 às 21:00</p>
-              <p>Sexta - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 1.6 - Estudo Sistematizado da Doutrina Espírita (ESDE) */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              1.6 - Estudo Sistematizado da Doutrina Espírita (ESDE)
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Quarta - 19:30 às 21:00</p>
-              <p>Sexta - 17:30 às 19:00</p>
-            </div>
-          </div>
-        </div>
-
-        {/* 2.0 - Atendimento Espiritual */}
-        <SectionTitle as="h3">2.0 - Atendimento Espiritual</SectionTitle>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-fit gap-4">
-          {/* 2.1 - Orientação Fraterna */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              2.1 - Orientação Fraterna
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 2.2 - Assistência Espiritual */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              2.2 - Assistência Espiritual
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Domingo - 16:30 às 19:00</p>
-              <p>Terça - 16:30 às 19:00, 19:30 às 21:00</p>
-              <p>Quinta - 19:30 às 21:00</p>
-            </div>
-          </div>
-
-          {/* 2.3 - Passe */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              2.3 - Passe
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Domingo - 16:30 às 19:00</p>
-              <p>Terça - 16:30 às 19:00, 19:30 às 21:00</p>
-              <p>Quinta - 19:30 às 21:00</p>
-            </div>
-          </div>
-
-          {/* 2.4 - Mediúnicas */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              2.4 - Mediúnicas
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Domingo - 16:30 às 19:00</p>
-              <p>Terça - 16:30 às 19:00, 19:30 às 21:00</p>
-              <p>Quinta - 19:30 às 21:00</p>
-            </div>
-          </div>
-        </div>
-
-        {/* 3.0 - Evangelização Infantojuvenil */}
-        <SectionTitle as="h3">3.0 - Evangelização Infantojuvenil</SectionTitle>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-fit gap-4">
-          {/* 3.1 - Grupo da Família */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              3.1 - Grupo da Família
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 3.2 - Grupo Musical Filhos de Assis */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              3.2 - Grupo Musical Filhos de Assis
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 3.3 - Eventos e Campanhas */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              3.3 - Eventos e Campanhas
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-        </div>
-
-        {/* 4.0 - Ação Social */}
-        <SectionTitle as="h3">4.0 - Ação Social</SectionTitle>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-fit gap-4">
-          {/* Programa Coração Fraterno */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              Programa Coração Fraterno
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 4.2 - Programa Arte Feliz */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              4.2 - Programa Arte Feliz
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 4.3 - Programa Renascer */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              4.3 - Programa Renascer
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-
-          {/* 4.4 - Programa Mão Amiga */}
-          <div className="p-4 flex flex-col gap-4 border border-black md:max-w-[350px]">
-            <SectionTitle as="h4" size="small">
-              4.4 - Programa Mão Amiga
-            </SectionTitle>
-            <div className="flex flex-col gap-2">
-              <p>Segunda - 19:30 às 21:00</p>
-              <p>Sábado - 17:30 às 19:00</p>
-            </div>
-          </div>
-        </div>
         {/* Livraria */}
         <SectionTitle size="large">Livraria</SectionTitle>
         <div className="flex flex-col gap-2">
@@ -267,3 +145,27 @@ export default function Horarios() {
     </Layout>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = encodeURIComponent(`*[_type == "activitiesDoc"]`);
+
+  const url = `${process.env.SANITY_URL}query=${query}`;
+
+  const data = await fetch(url).then((res) => res.json());
+
+  const atividadesData = data.result[0];
+
+  if (!atividadesData || atividadesData.length === 0) {
+    return {
+      props: {
+        atividadesData: {},
+      },
+    };
+  }
+
+  return {
+    props: {
+      atividadesData,
+    },
+  };
+};
