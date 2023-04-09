ec1b9e0f5e4dff347125bee4dcfb54ed4b771e70
hel rabelo <helrabelo@gmail.com>
2023-04-09 19:28:18 -0300

    add email

--- web/pages/contato.tsx
+++ web/pages/contato.tsx
@@ -1,21 +1,42 @@
-import { ChangeEvent, useState } from "react";
-import Layout from "../components/Layout";
-
+import {ChangeEvent, useState} from 'react';
+import Layout from '../components/Layout';
 import Button from '../components/Button';
+import classNames from 'classnames';
 
 export default function Contato() {
   const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
+    name: '',
+    email: '',
+    message: '',
   });
 
-  const updateForm = (field: string) => (value: ChangeEvent<HTMLInputElement>) =>
-    setForm({ ...form, [field]: [value] });
+  const [sending, setSending] = useState(false); // Add sending state
 
-  const submit = () => {
-    console.log('submitted');
-  }
+  const updateForm =
+    (field: string) => (value: ChangeEvent<HTMLInputElement>) =>
+      setForm({...form, [field]: value.target.value});
+
+  const submit = async () => {
+    setSending(true); // Set sending state to true when button is clicked
+    try {
+      const response = await fetch('/api/sendEmail', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(form),
+      });
+      const data = await response.json();
+      if (data.success) {
+        console.log('Email sent successfully!');
+        setForm({name: '', email: '', message: ''});
+      } else {
+        console.error('Failed to send email');
+      }
+    } catch (error) {
+      console.error('Failed to send email:', error);
+    }
+
+    setSending(false); // Set sending state back to false when message is sent
+  };
 
   return (
     <Layout>
@@ -27,7 +48,8 @@ export default function Contato() {
             <input
               className="input border border-[#333] w-full px-4 py-2"
               type="text"
-              onChange={updateForm("name")}
+              value={form.name}
+              onChange={updateForm('name')}
             />
           </div>
           <div className="email flex flex-col gap-2">
@@ -35,18 +57,26 @@ export default function Contato() {
             <input
               className="input border border-[#333] w-full px-4 py-2"
               type="text"
-              onChange={updateForm("email")}
+              value={form.email}
+              onChange={updateForm('email')}
             />
           </div>
           <div className="name flex flex-col gap-2">
             <p className="font-[900]">Mensagem</p>
-            <input
+            <textarea
               className="input border border-[#333] w-full min-h-[160px] px-4 py-2"
-              type="textarea"
-              onChange={updateForm("message")}
+              value={form.message}
+              onChange={updateForm('message')}
             />
           </div>
-          <Button onClick={submit} className="mx-auto">Enviar mensagem!</Button>
+          <Button
+            onClick={submit}
+            className={classNames('mx-auto', {'opacity-50 hover:not-allowed': sending})}
+            disabled={sending}
+          >
+            {sending ? 'Enviando...' : 'Enviar mensagem!'}{' '}
+            {/*Conditional rendering based on sending state*/}
+          </Button>
         </div>
       </div>
     </Layout>
