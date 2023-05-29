38012318a2f0c9347c89de7655388064d8d71ae0
hel rabelo <helrabelo@gmail.com>
2023-05-29 10:34:10 -0300

    fix: add safety check

--- web/pages/boletins.tsx
+++ web/pages/boletins.tsx
@@ -18,9 +18,9 @@ type BoletinsPageProps = {
 };
 
 const BoletinsPage = ({boletinsPageData}: BoletinsPageProps) => {
-  const [openYears, setOpenYears] = useState<number[]>([
-    boletinsPageData[0].year,
-  ]);
+  const [openYears, setOpenYears] = useState<number[]>(
+    Array.isArray(boletinsPageData) ? [boletinsPageData[0]?.year] : []
+  );
 
   const toggleYear = (year: number) => {
     if (openYears.includes(year)) {
